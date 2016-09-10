4a260169f42c0a099cb346ab59cea8121716b64d
Gina White <ginabythebay@gmail.com>
2016-09-10 11:16:39 -0700

    stutter less

--- csv/citi/citi.go
+++ csv/citi/citi.go
@@ -2,23 +2,23 @@ package citi
 
 import "github.com/ginabythebay/ledger-tools/csv/ops"
 
-var citiHeaders = []string{"cleared", "date", "payee", "amount", "credit"}
+var headers = []string{"cleared", "date", "payee", "amount", "credit"}
 
 const (
-	citiCleared int = iota
-	citiDate
-	citiPayee
-	citiAmount
-	citiCredit
+	cleared int = iota
+	date
+	payee
+	amount
+	credit
 )
 
 // Mutators returns the operations we want to perform on citibank csv files.
 func Mutators() []ops.Mutator {
 	return []ops.Mutator{
-		ops.ReplaceHeader(citiHeaders),
+		ops.ReplaceHeader(headers),
 		ops.StripNewlines,
-		ops.MoveAndNegateIfPresent(citiCredit, citiAmount),
-		ops.EnsureDollars(citiAmount),
-		ops.StripCommas(citiAmount),
+		ops.MoveAndNegateIfPresent(credit, amount),
+		ops.EnsureDollars(amount),
+		ops.StripCommas(amount),
 	}
 }
